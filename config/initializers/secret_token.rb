c082508ca78d9eb048e4c24278e28190daf9f604
cableray <cableraywire@gmail.com>
2011-11-07 16:52:04 -0700

    Initial Commit
    rails skeleton initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftweb::Application.config.secret_token = 'd41913bebf417d0d5dd5d7c3ea64aa63485a23cbbadafe21c640b928e851588d863e5316b8d18766c368de22e03248cde9ebe99cb94bc0571f3daca3e94e239b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ftweb::Application.config.session_store :cookie_store, key: '_ftweb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ftweb::Application.config.session_store :active_record_store
