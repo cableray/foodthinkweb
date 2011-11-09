9e40a39a769da792637a69ec8588f11ee0fe8bb1
cableray <cableraywire@gmail.com>
2011-11-09 01:25:43 -0700

    Use in-memory database for testing

---
+++ config/initializers/in_memory_db.rb
@@ -0,0 +1,16 @@
+#Initialize testing db in memory
+# TODO make this better code, and less hack-y
+
+def in_memory_database?
+  Rails.env == "test" and 
+    ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter || 
+    ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and
+    Rails.configuration.database_configuration['test']['database'] == ':memory:'
+end
+      
+if in_memory_database?
+  warn "creating sqlite in memory database"
+  $stdout=$stderr
+  load "#{Rails.root}/db/schema.rb"
+  $stdout=STDOUT
+end
